5c77241809b4ca2bff271477a62f4f2c044cb58e
avengers2405 <akshitmishra24@gmail.com>
2024-09-26 11:49:04 +0530

    minor update

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -10,7 +10,7 @@ export default async function EventsPage() {
         <>
             <section className="min-h-screen bg-secondary-800 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] bg-dark-bg">
                 <div className="mx-auto py-10 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
-                    <h2 className="mt-12 mb-4 px-4 text-4xl font-bold text-white">Events</h2>
+                    <h2 className="container mx-auto mt-12 mb-4 px-4 text-4xl font-bold text-white">Events</h2>
                     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
                         {/* Event cards here */}
